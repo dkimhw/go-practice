b23545ba8b055879b51f0b8ca487d8dcc440a965
David Kim <dkhw90@gmail.com>
2024-07-18 22:27:22 +0200

    Refactor hello function by creating greeting prefix func

--- hello/hello_world.go
+++ hello/hello_world.go
@@ -1,27 +1,30 @@
 package main
 
-import "fmt"
+const (
+	spanish = "Spanish"
+	french  = "French"
 
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
 
-func Hello(name string, lang string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	if lang == "English" {
-		return englishHelloPrefix + name
-	}
-
-	if lang == "Spanish" {
-		return spanishHelloPrefix + name
-	}
-
-	return frenchHelloPrefix + name
+	return greetingPrefix(language) + name
 }
 
-func main() {
-	fmt.Println(Hello("Chris", "English"))
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
