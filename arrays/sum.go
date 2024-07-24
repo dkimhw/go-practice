979c66816e6d99acecc1995607d411b5c48f780c
David Kim <dkhw90@gmail.com>
2024-07-24 20:38:30 +0200

    More array, slices practice with TDD

--- arrays/sum.go
+++ arrays/sum.go
@@ -14,11 +14,27 @@ func Sum(arr []int) int {
 }
 
 func SumAll(numsToSum ...[]int) []int {
-	var length int = len(numsToSum)
-	var result []int = make([]int, length)
+	var result []int
 
-	for i, nums := range numsToSum {
-		result[i] = Sum(nums)
+	for _, nums := range numsToSum {
+		result = append(result, Sum(nums))
+	}
+
+	return result
+}
+
+func SumAllTails(numsToSum ...[]int) []int {
+	var result []int
+	for _, nums := range numsToSum {
+		var tail []int
+
+		if len(nums) == 0 {
+			tail = []int{0}
+		} else {
+			tail = nums[1:]
+		}
+
+		result = append(result, Sum(tail))
 	}
 
 	return result
