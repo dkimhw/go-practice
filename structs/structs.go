f40894c23d368f360dd54068375bb7a4fc61d552
David Kim <dkhw90@gmail.com>
2024-07-25 22:12:15 +0200

    Practice with structs & methods

---
+++ structs/structs.go
@@ -0,0 +1,26 @@
+package structs
+
+import "math"
+
+// A struct is just a named collection of fields where you can store data.
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
