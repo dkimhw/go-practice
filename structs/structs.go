6c168040c323f5e935110011c691faa65a6ff1f5
David Kim <dkhw90@gmail.com>
2024-07-27 21:20:06 +0200

    Work on adding a new shape - triangle to testing file

--- structs/structs.go
+++ structs/structs.go
@@ -6,7 +6,18 @@ type Shape interface {
 	Area() float64
 }
 
-// A struct is just a named collection of fields where you can store data.
+/*
+A struct is just a named collection of fields where you can store data.
+
+Declaring structs to create your own data types which lets you bundle related data together
+and make the intent of your code clearer
+
+Declaring interfaces so you can define functions that can be used by different types (parametric polymorphism)
+
+# Adding methods so you can add functionality to your data types and so you can implement interfaces
+
+Table driven tests to make your assertions clearer and your test suites easier to extend & maintain
+*/
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -22,6 +33,15 @@ type Circle struct {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
 }
 
 func Perimeter(rectangle Rectangle) float64 {
--- structs/structs_test.go
+++ structs/structs_test.go
@@ -14,18 +14,23 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
+		{"Rectangle", Rectangle{12, 6}, 72.0},
+		{"Circle", Circle{10}, 314.1592653589793},
+		{"Triangle", Triangle{12, 6}, 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		want := tt.want
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			want := tt.want
+
+			if got != want {
+				t.Errorf("got %g want %g", got, want)
+			}
+		})
 	}
 }
