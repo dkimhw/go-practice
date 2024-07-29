679c36d84d4a18e65840e4529a932a2ce8cbecfd
David Kim <dkhw90@gmail.com>
2024-07-29 18:17:32 +0200

    Add testing for insufficient funds withdrawl

--- pointers/pointers_test.go
+++ pointers/pointers_test.go
@@ -11,6 +11,18 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
+	assertError := func(t *testing.T, got, want error) {
+		t.Helper()
+
+		if got == nil {
+			t.Fatal("didn't get an error but wanted one")
+		}
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
+
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
@@ -23,5 +35,14 @@ func TestWallet(t *testing.T) {
 		wallet.Withdraw(Bitcoin(10))
 		want := Bitcoin(10)
 		assertBalance(t, wallet, want)
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertError(t, err, ErrInsufficientFunds)
+		assertBalance(t, wallet, startingBalance)
 	})
 }
