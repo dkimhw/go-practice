4c8cf6ee459e28e62688877bad0284c052fc157c
David Kim <dkhw90@gmail.com>
2024-08-01 01:57:49 +0200

    More practice with maps & testing - refactored dictionary & dictionary err

--- map/dictionary.go
+++ map/dictionary.go
@@ -1,7 +1,62 @@
 package dictionary
 
+/*
+An interesting property of maps is that you
+can modify them without passing as an address to it (e.g &myMap).
+
+A map value is a pointer to a runtime.hmap structure.
+
+So when you pass a map to a function/method, you are indeed copying it,
+but just the pointer part, not the underlying data structure that contains the data.
+
+A gotcha with maps is that they can be a nil value.
+A nil map behaves like an empty map when reading, but attempts to write
+to a nil map will cause a runtime panic.
+
+Therefore, you should never initialize a nil map variable: `var m map[string]string`
+
+Instead, you can initialize an empty map or use the make keyword to create a map for you:
+
+var dictionary = map[string]string{}
+var dictionary = make(map[string]string)
+
+Both approaches create an empty hash map and point dictionary at it.
+Which ensures that you will never get a runtime panic.
+*/
+type DictionaryErr string
+
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
+)
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
 type Dictionary map[string]string
 
-func Search(d Dictionary, word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
