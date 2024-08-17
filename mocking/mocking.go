07563add12e93a06ffb8197b7b7b0ec1b0ede5b6
David Kim <dkhw90@gmail.com>
2024-08-17 14:05:42 +0200

    Practice mocking in golang

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -5,19 +5,80 @@ import (
 	"io"
 	"os"
 	"strconv"
+	"time"
 )
 
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+/*
+Spies are a kind of mock which can record how a dependency is used.
+They can record the arguments sent in, how many times it has been called, etc.
+In our case, we're keeping track of how many times Sleep() is called
+so we can check it in our test.
+*/
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+/*
+Our SpyCountdownOperations implements both io.Writer and Sleeper,
+recording every call into one slice. In this test we're only concerned
+about the order of operations, so just recording them as list of named
+operations is sufficient.
+*/
+
+const write = "write"
+const sleep = "sleep"
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i >= 1; i-- {
 		fmt.Fprint(out, strconv.Itoa(i)+"\n")
+		sleeper.Sleep()
 	}
 
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
 }
