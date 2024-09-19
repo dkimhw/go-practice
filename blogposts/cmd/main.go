cd8b643bffc6598370bde3f0b211b0dc13e40d62
David Kim <dkhw90@gmail.com>
2024-09-19 20:30:26 +0200

    Add parsing logic for tags & body

---
+++ blogposts/cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"os"
+	"practice/blogposts"
+)
+
+func main() {
+	posts, err := blogposts.NewPostsFromFS(os.DirFS("posts"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(posts)
+}
---
+++ blogposts/cmd/posts/hi.md
@@ -0,0 +1,10 @@
+Title: Post 1
+Description: Description 1
+Tags: tdd, go
+
+---
+
+Hello
+World
+From
+Spain
